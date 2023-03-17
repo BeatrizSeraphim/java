29bfbc868fe8c364e6c7714aacf57acf2f60a9a7
Beatriz Seraphim <beatrizoseraphim@gmail.com>
2023-03-17 18:24:23 -0300

    Atualização

--- aula02/src/aula02/Exe02.java
+++ aula02/src/aula02/Exe02.java
@@ -5,35 +5,37 @@ import java.util.Scanner;
 public class Exe02 {
 
 	public static void main(String[] args) {
-		Scanner leia = new Scanner(System.in);
+		 Scanner leia = new Scanner(System.in);
 
-		String nome, pergunta;
-		int idade;
-		boolean doacao = false;
+	        int idade;
+	        String nome;
+	        char pergunta;
+	        boolean doacao = false;
 
-		System.out.println("Digite o seu Nome: ");
-		nome = leia.nextLine();
+	        System.out.println("Digite o Nome do Doador: ");
+	        nome = leia.nextLine();
 
-		System.out.println("Digite a sua Idade: ");
-		idade = leia.nextInt();
+	        System.out.println("Digite a Idade do Doador: ");
+	        idade = leia.nextInt();
 
-		System.out.println("Primeira doação? (S/N)");
-		leia.skip("\\R?");
-		pergunta = leia.nextLine().toUpperCase();
+	        System.out.println("Primeira doação de Sangue? (S/N): ");
+	        leia.skip("\\R?");
+	        pergunta = leia.nextLine().charAt(0);
 
-		if (pergunta == "S")
-			doacao = true;
+	        if (pergunta == 's')
+	            doacao = true;
 
-		if (idade >= 60 && idade <= 69) {
-			if (doacao)
-				System.out.println(nome + " não está apto(a) a doar!");
-			else
-				System.out.println(nome + " está apto(a) a doar!");
-		} else if (idade < 18 || idade > 69)
-			System.out.println(nome + " não está apto(a) a doar!");
-		else
-			System.out.println(nome + " está apto(a) a doar!");
+	        if (idade >= 60 && idade <= 69) {
+	            if (doacao)
+	                System.out.println(nome + " você não está apto(a) para doar sangue!");
 
+	            else
+	                System.out.println(nome + " você está apto(a) para doar sangue!");
+	        } else if (idade < 18 || idade > 69)
+	            System.out.println(" você não está apto(a) para doar sangue!");
+
+	        else if (idade >= 18 && idade <= 59)
+	            System.out.println(nome + " você está apto(a) para doar sangue!");
+
+	    }
 	}
-
-}
